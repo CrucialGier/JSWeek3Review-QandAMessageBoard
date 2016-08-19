f538de7ea4baab29d5b4a21f75a19e954f62d10e
Stewart Cole <stewart@>
2016-08-19 09:37:52 -0700

    create show and hide answers functionality for each question display component and minimal syling

--- app/components/question-display.js
+++ app/components/question-display.js
@@ -2,12 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   isSelected: false,
+  displayAnswers: false,
   actions: {
     showDetails: function(){
       this.set('isSelected', true);
     },
     hideDetails: function(){
       this.set('isSelected', false);
+    },
+    showAnswers: function(){
+      this.set('displayAnswers', true);
+    },
+    hideAnswers: function(){
+      this.set('displayAnswers', false);
     }
   }
 });
